d287988f733138006da91c4a8d60ef50737e2283
DANIEL CHEN <daniel77chen@hotmail.com>
2018-05-01 12:29:51 -0400

    add some error checking in build script

--- build.bat
+++ build.bat
@@ -1,13 +1,14 @@
-for /f "delims=" %%A in ('node compress.js %1') do set "ver=%%A"
-
-IF %ERRORLEVEL% == 0 (
-	DEL "Chrome Tools.zip"
-	7z a "Chrome Tools.zip" minified -tzip -sdel
-	echo "build %ver%"
-	git reset
-	git add manifest.json "Chrome Tools.zip"
-	git commit -m "build %ver%"
-	git tag "%ver%"
-) ELSE (
-	rd /s /q minified
+setlocal EnableDelayedExpansion
+for /f "delims=" %%A in ('node compress.js %1') do (
+	IF !errorlevel! == 0 IF NOT "%%A" == "" (
+		DEL "Chrome Tools.zip"
+		7z a "Chrome Tools.zip" minified -tzip -sdel
+		echo "build %%A"
+		git reset
+		git add manifest.json "Chrome Tools.zip"
+		git commit -m "build %%A"
+		git tag "%%A"
+	) ELSE (
+		rd /s /q minified
+	)
 )
