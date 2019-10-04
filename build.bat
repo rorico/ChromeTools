1bf4cba380cd7f6a3509ce8f4d76b250b9d02e91
Daniel Chen <rorico@users.noreply.github.com>
2019-10-03 22:35:09 -0700

    fix build script

--- build.bat
+++ build.bat
@@ -6,7 +6,7 @@ IF "%inc%" == "" (
 for /f "delims=" %%A in ('node compress.js %inc%') do (
 	IF !errorlevel! == 0 IF NOT "%%A" == "" (
 		DEL "Chrome Tools.zip"
-		lib\7z a "Chrome Tools.zip" minified -tzip -sdel
+		7z a "Chrome Tools.zip" minified -tzip -sdel
 		echo "build %%A"
 		git reset
 		git add manifest.json "Chrome Tools.zip"
